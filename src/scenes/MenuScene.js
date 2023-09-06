c912d9c082d10cf4ebe710e4fcb336618310f49d
yudiz-fenil <fenil.babariya@yudiz.com>
2023-09-06 14:21:09 +0530

    Features Added

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -16,6 +16,18 @@ class MenuScene extends Phaser.Scene {
 	/** @returns {void} */
 	editorCreate() {
 
+		// PvsP_Highlight
+		const pvsP_Highlight = this.add.image(763, 830, "White-Highlight");
+		pvsP_Highlight.scaleX = 0.9;
+		pvsP_Highlight.scaleY = 0.9;
+		pvsP_Highlight.visible = false;
+
+		// PvsC_Highlight
+		const pvsC_Highlight = this.add.image(1214, 827, "White-Highlight");
+		pvsC_Highlight.scaleX = 0.9;
+		pvsC_Highlight.scaleY = 0.9;
+		pvsC_Highlight.visible = false;
+
 		// logo_With_Roap
 		this.add.image(960, 510, "Logo-With-Roap");
 
@@ -25,12 +37,18 @@ class MenuScene extends Phaser.Scene {
 		// player_VS_Player
 		const player_VS_Player = this.add.image(748, 818, "Player-VS-Player");
 
+		this.pvsP_Highlight = pvsP_Highlight;
+		this.pvsC_Highlight = pvsC_Highlight;
 		this.player_VS_Computer = player_VS_Computer;
 		this.player_VS_Player = player_VS_Player;
 
 		this.events.emit("scene-awake");
 	}
 
+	/** @type {Phaser.GameObjects.Image} */
+	pvsP_Highlight;
+	/** @type {Phaser.GameObjects.Image} */
+	pvsC_Highlight;
 	/** @type {Phaser.GameObjects.Image} */
 	player_VS_Computer;
 	/** @type {Phaser.GameObjects.Image} */
@@ -75,6 +93,19 @@ class MenuScene extends Phaser.Scene {
 				} 
 			  });
 		})
+
+		this.player_VS_Player.on("pointerover",()=>
+		{
+			this.pvsP_Highlight.setVisible(true)
+			this.pvsC_Highlight.setVisible(false)
+		})
+		this.player_VS_Computer.on("pointerover",()=>
+		{
+			this.pvsP_Highlight.setVisible(false)
+			this.pvsC_Highlight.setVisible(true)
+		})
+
+
 
 	}
 
